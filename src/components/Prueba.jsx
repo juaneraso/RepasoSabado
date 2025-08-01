d22deaff953b9ad02a3b9cca783ad1c2e49abd1c
juaneraso <juangui9508@hotmail.com>
2025-08-01 14:02:07 -0500

    integracion back

--- src/components/Prueba.jsx
+++ src/components/Prueba.jsx
@@ -5,12 +5,13 @@ function Prueba() {
   const [datos, setDatos] = useState([]);
   const [usuario, setUsuario] = useState("");
 
+  //VARIABLE DE SU BACKEND
+  const BACK = import.meta.env.VITE_APP_BACK;
+
   useEffect(() => {
     const fetchDatos = async () => {
       try {
-        const respuesta = await fetch(
-          "https://jsonplaceholder.typicode.com/users/"
-        );
+        const respuesta = await fetch(BACK);
 
         const datosObtenidos = await respuesta.json();
         setDatos(datosObtenidos);
@@ -26,7 +27,7 @@ function Prueba() {
   console.log("Datos:", datos);
   // console.log("PosicionDatoCero:", datos[0].name);
 
-  console.log("UsuarioPrueba:", datos[0]?.name);
+  console.log("UsuarioPrueba:", datos[0]?.first_name);
   console.log("Usuario:", usuario);
 
   const aumentar = () => {
@@ -49,9 +50,10 @@ function Prueba() {
       <p>{numero}</p>
       <h2>Datos Usuario</h2>
       <label>Usuario:</label>
-      <h3>{datos[0]?.name}</h3>
-      <label>Usuario:</label>
-      <h4>{usuario}</h4>
+      <h3>{datos[2]?.first_name}</h3>
+      <h4>{datos[2]?.family_name}</h4>
+      {/* <label>Usuario:</label>
+      <h4>{usuario}</h4> */}
     </>
   );
 }
